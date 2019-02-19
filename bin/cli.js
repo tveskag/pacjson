6e2fb92916a193c8484dab7bb21d83adfc9ec37c
Asger V K Nielsen <Asgervkn@gmail.com>
2019-02-19 16:59:11 +0100

    using http request directly to npm registry instead of npm outdated

--- bin/cli.js
+++ bin/cli.js
@@ -1,61 +1,96 @@
 #!/usr/bin/env node
-const { spawn } = require('child_process');
 const readline = require('readline');
 const path = require('path');
+// const fs = require('fs');
+const http = require('http');
 
 const [,, ...args] = process.argv;
 
-const writeOut = (dependency, version, line) => {
-	process.stdout.write(`E:123:${line}:${dependency.index}:${dependency[1]} latest version: ${version}\n`);
+const writeOut = (message, line, col) => {
+    process.stdout.write(`
+        E:${line}:${col}:${message}\n
+    `);
 };
-const getLines = (list) => {
+const request = (dep, cb) => {
+    const opt = {
+        host: "registry.npmjs.org",
+        path: `/${dep}`,
+        method: "GET",
+        headers: {
+            "Accept": "application/vnd.npm.install-v1+json"
+        }
+    };
+    http.get(opt, (res) => {
+        const { statusCode } = res;
+        if (statusCode !== 200) {
+            res.destroy();
+            return cb(true, null);
+        }
+        let raw = '';
+        res.on('data', (chunk) => raw += chunk);
+        return res.on('end', () => {
+            return cb(false, raw);
+        });
+    });
+
+}
+const checkVersion = (dep, version) => {
+    const versionCompare = (current, latest) => {
+        const cur = /([0-9]+)\.([0-9]+)\.([0-9]+)/.exec(current);
+        const lat = /([0-9]+)\.([0-9]+)\.([0-9]+)/.exec(latest);
+        return lat[1] > cur[1] || lat[2] > cur[2] || lat[3] > cur[3];
+    }
+    return new Promise(resolve => {
+        request(dep, (err, raw) => {
+            if (err) {
+                return resolve(`failed to fetch latest version of ${dep}`);
+            }
+            const {latest} = JSON.parse(raw)['dist-tags'];
+            let out = null;
+            if (versionCompare(version, latest)) {
+                out = `${dep} latest version: ${latest}`;
+            }
+            resolve(out);
+        });
+    });
+};
+const getLines = () => {
 	const rl = readline.createInterface({
 		input: process.stdin,
 		output: process.stdout
 	});
 
-	let inModule = false;
 	const modulesStart = /"dependencies"|"devDependencies"/;
-	const extractDep = /"([^"]+)"/;
+	const extractDep = /"([^"]+)": ".?([0-9]+\.[0-9]+\.[0-9]+)"/;
 
-	const checkVersion = (dep) => {
-		const reg = new RegExp(`${dep}\\s*([0-9]+\\.[0-9]+\\.[0-9]+)\\s*([0-9]+\\.[0-9]+\\.[0-9]+)\\s*([0-9]+\\.[0-9]+\\.[0-9]+)`, 'g');
-		return reg.exec(list);
-	};
-	let i = 0;
+	let inModule = false;
+    const handleLine = async (line, i) => {
+        if (inModule) {
+            const depExtract = extractDep.exec(line); // do more destructuring
+            if (depExtract) {
+                const col = depExtract.index;
+                const [, dependency, version] = depExtract;
+                const message = await checkVersion(dependency, version);
+                if (message) {
+                    writeOut(message, i, col);
+                }
+            } else {
+                inModule = false;
+            }
+        } else {
+            inModule = modulesStart.test(line);
+        }
+    };
 	const mainloop = async () => {
+        let i = 0;
 		for await (const line of rl) {
 			i++;
-			if (inModule) {
-				const dependency = extractDep.exec(line);
-				if (dependency) {
-					const versions = checkVersion(dependency[1]);
-					if (versions) {
-						writeOut(dependency, versions[3], i);
-					}
-				} else {
-					inModule = false;
-				}
-			} else {
-				inModule = modulesStart.test(line);
-			}
+            handleLine(line, i);
 		}
 	}
 	mainloop();
 };
 
-const spawnChild = () => {
-	const npm = spawn('npm', ['outdated']);
-	let outdated = "";
-
-	npm.stdout.on('data', (data) => {
-		outdated += data;
-	});
-	npm.on('close', (signal) => {
-		// if (signal == bad) terminate;
-		getLines(outdated);
-	});
-};
 if (path.basename(args[0]) === 'package.json') {
-	spawnChild();
+	getLines();
 }
